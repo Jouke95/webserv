cf1699fdb46cee3abed34346f991d27186a14a6c
Jouke Spanninga <jspannin@f0r4s6.codam.nl>
2026-03-26 12:17:28 +0100

    server hpp

--- includes/Server.hpp
+++ includes/Server.hpp
@@ -2,26 +2,40 @@
 #define SERVER_HPP
 
 #include <iostream>
-
-class Server
-{
-private:
-	int			_serverFileDescriptor;
-	int			_clientFileDescriptor;
-	
-public:
-	Server();
-	~Server();
-	Server(const Server& other) = delete;
-	const Server& operator=(const Server& other) = delete;
-
-	// Methods
-	int start();
-	int waitForRequest();
-	
-};
+#include <vector>
+#include <poll.h>
+#include "Client.hpp"
 
 #define PORT 8080
 #define BUFFER_SIZE 10000
+
+class Server {
+	private:
+		int	_serverFileDescriptor;
+
+		struct Connection {
+			struct pollfd pfd;
+			Client client;
+			bool isServer;
+		};
+
+		std::vector<Connection> _connections;
+
+		// Methods
+		void myPoll();
+		void addConnection(std::vector<Connection>& connections);
+		bool handleRequest(Connection &conn);
+		void sendResponse(Connection &conn);
+
+	public:
+		Server();
+		~Server();
+		Server(const Server& other) = delete;
+		const Server& operator=(const Server& other) = delete;
+
+		// Methods
+		int start();
+		int run();
+};
 
 #endif
