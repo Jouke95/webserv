ef9b0c14f8f624f98f1fb73e5a630554a7ae5533
Jouke Spanninga <jspannin@f0r4s6.codam.nl>
2026-03-26 12:17:08 +0100

    Server changes

--- src/Server.cpp
+++ src/Server.cpp
@@ -1,3 +1,5 @@
+#include <vector>
+#include <poll.h>
 #include <cstdio>
 #include <iostream>
 #include <Server.hpp>
@@ -8,10 +10,7 @@
 
 #include "endpoints/CommonGatewayInterface.hpp"
 
-Server::Server() : _serverFileDescriptor(-1), _clientFileDescriptor(-1)
-{
-	
-}
+Server::Server() : _serverFileDescriptor(-1) {}
 
 Server::~Server()
 = default;
@@ -20,8 +19,7 @@ int Server::start()
 {
 	_serverFileDescriptor = socket(AF_INET, SOCK_STREAM, 0);
 
-	if (_serverFileDescriptor < 0)
-	{
+	if (_serverFileDescriptor < 0) {
 		perror("socket");
 		return 1;
 	}
@@ -34,13 +32,12 @@ int Server::start()
 	addr.sin_addr.s_addr = INADDR_ANY;
 	addr.sin_port        = htons(PORT);
 	
-	if (bind(_serverFileDescriptor, (sockaddr*)&addr, sizeof(addr)) < 0)
-	{
+	if (bind(_serverFileDescriptor, (sockaddr*)&addr, sizeof(addr)) < 0) {
 		perror("bind");
 		return 1;
 	}
-	if (listen(_serverFileDescriptor, 10) < 0)
-	{
+
+	if (listen(_serverFileDescriptor, 10) < 0) {
 		perror("listen");
 		return 1;
 	}
@@ -48,41 +45,101 @@ int Server::start()
 	return 0;
 }
 
-int Server::waitForRequest()
+int Server::run()
 {
-	while (true)
-	{
-		_clientFileDescriptor = accept(_serverFileDescriptor, nullptr, nullptr);
-		if (_clientFileDescriptor < 0)
-		{
-			perror("accept");
-			continue;
+	Connection serverConn;
+	serverConn.pfd.fd = _serverFileDescriptor;
+	serverConn.pfd.events = POLLIN;
+	serverConn.pfd.revents = 0;
+	serverConn.isServer = true;
+
+	_connections.push_back(serverConn);
+
+	while (true) {
+
+		myPoll();
+
+		for (size_t i = 0; i < _connections.size(); i++) {
+			if (_connections[i].pfd.revents & POLLIN) {
+				if (_connections[i].isServer)
+					addConnection(_connections);
+				else {
+					if (handleRequest(_connections[i]) == false) {
+						_connections.erase(_connections.begin() + i);
+						i--;
+					}
+				}
+			}
+			else if (_connections[i].pfd.revents & POLLOUT) {
+				sendResponse(_connections[i]);
+				_connections.erase(_connections.begin() + i);
+				i--;
+			}
 		}
-		std::cout << "Connection made.\n\n";
-		char buf[BUFFER_SIZE] = {};
-		read(this->_clientFileDescriptor, buf, BUFFER_SIZE - 1);
-
-		std::string request = buf;
-		
-		std::cout << request << std::endl;
-		
-		Parser parser(request);
-
-		parser.buildRequest();
-		HttpRequest httpRequest = parser.getRequest();
-		HttpResponse httpResponse = parser.getResponse();
-
-		std::cout << "Request Method: " << httpRequest.getMethod() << std::endl;
-		CommonGatewayInterface CGI(httpRequest, httpResponse);
-
-		httpResponse = CGI.Redirect();
-		
-		std::cout << "Response: " << httpResponse.getStatusMessage() << std::endl;
-		parser.setResponse(httpResponse);
-		std::string response = parser.buildResponseString();
-
-		write(this->_clientFileDescriptor, response.c_str(), response.size());
-		close(this->_clientFileDescriptor);
-		std::cout << "Connection closed.\n";
 	}
 }
+
+bool Server::handleRequest(Connection &conn) {
+	char buf[BUFFER_SIZE] = {};
+	int bytesRead = read(conn.pfd.fd, buf, BUFFER_SIZE - 1);
+	if (bytesRead <= 0) {
+		close(conn.pfd.fd);
+		return false;
+	}
+	Parser parser(buf);
+	conn.client._response = parser.buildResponseString();
+	std::cout << conn.client._response << std::endl;
+	conn.pfd.events = POLLIN | POLLOUT;
+	return true;
+}
+
+void Server::sendResponse(Connection &conn) {
+	write(conn.pfd.fd, conn.client._response.c_str(), conn.client._response.size());
+	close(conn.pfd.fd);
+}
+
+void Server::myPoll() {
+	std::vector<pollfd> fds;
+
+	for (size_t i = 0; i < _connections.size(); i++)
+		fds.push_back(_connections[i].pfd);
+
+	poll(fds.data(), fds.size(), -1);
+
+	for (size_t i = 0; i < _connections.size(); i++)
+		_connections[i].pfd.revents = fds[i].revents;
+}
+
+void Server::addConnection(std::vector<Connection>& connections) {
+	int _clientFileDescriptor = accept(_serverFileDescriptor, nullptr, nullptr);
+	std::cout << "Connection made.\n\n";
+
+	Connection newClient;
+
+	newClient.pfd.fd = _clientFileDescriptor;
+	newClient.pfd.events = POLLIN;
+	newClient.pfd.revents = 0;
+	newClient.client._response = "";
+	newClient.isServer = false;
+
+	connections.push_back(newClient);
+}
+
+
+
+
+
+
+
+
+// int poll(struct pollfd *fds, nfds_t nfds, int timeout);
+
+// struct pollfd {
+// 	int   fd;			// welke file descriptor?
+// 	short events;		// wat wil IK monitoren?
+// 	short revents;		// wat meldt het OS terug?
+// };
+
+// fds[0].fd		= listening_fd;		// de fd van socket()
+// fds[0].events	= POLLIN;			// ik wil weten als er iets te lezen is
+// fds[0].revents	= 0;				// dit vult het OS in, jij zet het op 0
